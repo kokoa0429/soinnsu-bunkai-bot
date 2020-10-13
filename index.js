1bed1df5d20cbee40aa3d56e0efdc1b2dd43edb3
kokoa0429 <kokoa@ciebus.net>
2020-10-13 17:54:14 +0900

    たくさんのサーバーに対応した

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const fs = require('fs')
 const client = new Discord.Client()
 const token = JSON.parse(fs.readFileSync('token.json', 'utf8')).token
 
-let num = 0
-let gaming = false
+let num = new Map()
+let gaming = new Map()
 
 let connection
 
@@ -26,27 +26,31 @@ client.on("message", message => {
         if (message.content.startsWith(".i")) {
             const args = message.content.slice(2).trim().split(/ +/)
             if (args[0] === "new") {
-                num = newNumber(10000)
-                channel.send(num + "を素因数分解してください")
-                gaming = true
+                num.set(channel.id,newNumber(10000))
+                channel.send(num.get(channel.id) + "を素因数分解してください")
+                gaming.set(channel.id, true)
+            } else if(args[0] === "help"){
+                channel.send(".i new でげーむがかいしできます \n.i [数字]で因数分解できます")
             }
-            else if (!isNaN(args[0]) && gaming) {
+            else if (!isNaN(args[0]) && gaming.get(channel.id)) {
                 const n = Number(args[0])
+                let nnum = num.get(channel.id)
                 if (n <= 1) {
                     return;
                 }
                 else {
-                    if (num % n == 0) {
+                    if (nnum % n == 0) {
                         if (!isPrimeNumber(n)) {
                             channel.send("素数じゃないようです。。。")
                         } else {
-                            num /= n
-                            channel.send(args[0] + "で割れました。商:" + num)
+                            nnum /= n
+                            channel.send(args[0] + "で割れました。商:" + nnum)
+                            num.set(channel.id,nnum)
                         }
                     }
-                    if (num == 1) {
+                    if (nnum == 1) {
                         channel.send("ゲームクリアー わーい")
-                        gaming = false
+                        gaming.set(channel.id, false)
                     }
                 }
             }
